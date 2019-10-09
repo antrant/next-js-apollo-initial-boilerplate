13a84b326a8a03eac49949a8a96cbfb8c52aa100
antrant <antran.inbox@gmail.com>
2019-10-09 17:04:27 +0700

    Add delete button

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
-import {Paper, FormControlLabel, makeStyles, Checkbox} from '@material-ui/core';
+import {
+  Paper,
+  FormControlLabel,
+  makeStyles,
+  Checkbox,
+  Button,
+  Grid,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(1, 2),
     marginBottom: theme.spacing(1),
+  },
+  deleteButton: {
+    marginTop: theme.spacing(1),
   },
 }));
 
@@ -15,13 +25,22 @@ const TodoList = ({todos}) => {
     <>
       {todos.map((todo) => (
         <Paper key={todo.id} className={classes.root}>
-          <FormControlLabel
-            value={todo.title}
-            control={<Checkbox color="primary" />}
-            label={todo.title}
-            labelPlacement="end"
-            checked={todo.completed}
-          />
+          <Grid container spacing={0}>
+            <Grid item xs={10}>
+              <FormControlLabel
+                value={todo.title}
+                control={<Checkbox color="primary"/>}
+                label={todo.title}
+                labelPlacement="end"
+                checked={todo.completed}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <Button size="small" className={classes.deleteButton}>
+                Delete
+              </Button>
+            </Grid>
+          </Grid>
         </Paper>
       ))}
     </>
